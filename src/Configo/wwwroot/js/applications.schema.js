e5e8961e5136ad5c9e8f59541de79ab3a8b334af
Alexander Moerman <alexander.moerman@gmail.com>
2023-10-27 08:03:25 +0200

    Try to set up monaco editor Fail miserably

---
+++ src/Configo/wwwroot/js/applications.schema.js
@@ -0,0 +1,12 @@
+﻿// use strict
+
+window.applications = window.applications ?? {};
+window.applications.schema = {
+    initializeEditor: () => {
+        const container = document.getElementById("editor-container");
+        const editor = monaco.editor.create(container, {
+            value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+            language: 'javascript'
+        });
+    }
+};
