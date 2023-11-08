7126350a6f0a76f81e6eaeef923e2de11e391a6e
Alexander Moerman <alexander.moerman@gmail.com>
2023-11-08 13:49:16 +0100

    Add early implementation for saving & getting vars

--- src/Configo/Domain/Variables.cs
+++ src/Configo/Domain/Variables.cs
@@ -18,6 +18,9 @@ public sealed record VariableModel
 public sealed record VariablesEditModel
 {
     public required string Json { get; set; }
+    
+    public required List<int> ApplicationIds { get; set; }
+    
     public required List<int> TagIds { get; set; }
 }
 
@@ -26,87 +29,142 @@ public sealed class VariableManager
     private readonly IDbContextFactory<ConfigoDbContext> _dbContextFactory;
     private readonly ILogger<VariableManager> _logger;
     private readonly VariablesJsonDeserializer _deserializer;
+    private readonly VariablesJsonSerializer _serializer;
 
     public VariableManager(
         IDbContextFactory<ConfigoDbContext> dbContextFactory,
         ILogger<VariableManager> logger,
-        VariablesJsonDeserializer deserializer)
+        VariablesJsonDeserializer deserializer,
+        VariablesJsonSerializer serializer)
     {
         _dbContextFactory = dbContextFactory ?? throw new ArgumentNullException(nameof(dbContextFactory));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _deserializer = deserializer ?? throw new ArgumentNullException(nameof(deserializer));
+        _serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
     }
 
-    public async Task<List<VariableModel>> GetConfigAsync(int apiKeyId, CancellationToken cancellationToken)
+    public async Task<string> GetConfigAsync(int apiKeyId, CancellationToken cancellationToken)
     {
         await using var dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
 
         _logger.LogDebug("Getting config for API key {ApiKeyId}", apiKeyId);
 
+        var applications = dbContext.Applications;
+        var apiKeys = dbContext.ApiKeys;
+        var tags = dbContext.Tags;
+        var apiKeyTags = dbContext.ApiKeyTags;
+        var variables = dbContext.Variables;
+        var tagVariables = dbContext.TagVariables;
+        var applicationVariables = dbContext.ApplicationVariables;
+        
+        // Get API key
+        var apiKey = await apiKeys.SingleAsync(a => a.Id == apiKeyId, cancellationToken);
+        
         // Get tags of API key
-        var tags = await dbContext.Tags
-            .GroupJoin(dbContext.ApiKeyTags.Where(apiKeyTag => apiKeyTag.ApiKeyId == apiKeyId),
-                t => t.Id,
-                a => a.TagId,
-                (tag, _) => tag)
+        var tagIdsOfApiKey = await tags
+            .Where(tag => apiKeyTags.Any(at => at.TagId == tag.Id && at.ApiKeyId == apiKeyId))
+            .Select(tag => tag.Id)
             .ToListAsync(cancellationToken);
-
-        var tagIds = tags.Select(t => t.Id).ToList();
-
-        // Get variables that are linked to ALL tags
-        var variables = await dbContext.TagVariables
-            .Where(tv => tagIds.Contains(tv.TagId))
-            .GroupBy(tv => tv.VariableId)
-            .Where(g => g.Count() == tags.Count)
-            .Select(tv => tv.Key)
-            .Join(dbContext.Variables,
-                variableId => variableId,
-                variable => variable.Id,
-                (variableId, variable) => variable)
-            .Select(variable => new VariableModel
+        
+        // Get variables of API key
+        var variablesOfApiKey = await variables
+            // If a variable is linked to any other tag than the API key tags, then it is not relevant to this API key
+            .Where(v => !tagVariables.Any(tv => tv.VariableId == v.Id && !tagIdsOfApiKey.Contains(tv.TagId)))
+            // If a variable is linked to one or more applications and the current application is not one of them, then it is not relevant to this API key
+            .Where(v => !applicationVariables.Any(av => av.VariableId == v.Id)
+                        || applicationVariables.Any(av => av.VariableId == v.Id && av.ApplicationId == apiKey.ApplicationId))
+            // For each variable, we want to calculate a specificity to decide which variable overrides which other variable
+            .Select(v => new
             {
-                Key = variable.Key,
-                Value = variable.Value,
-                ValueType = variable.ValueType
+                Variable = new VariableModel
+                {
+                    Key = v.Key,
+                    Value = v.Value,
+                    ValueType = v.ValueType
+                },
+                Specificity = tagVariables.Count(tv => tv.VariableId == v.Id && tagIdsOfApiKey.Contains(tv.TagId))
+                    + applicationVariables.Count(av => av.VariableId == v.Id && av.ApplicationId == apiKey.Id)
             })
             .ToListAsync(cancellationToken);
+        
+        // Take into account that we might have overlapping variables with the same key. In that case, the most "specific" variable wins
+        var variablesOfApiKeyWithHighestSpecificity = variablesOfApiKey
+            .GroupBy(v => v.Variable.Key)
+            .Select(g => g.MaxBy(v => v.Specificity)!.Variable)
+            .ToList();
 
         _logger.LogInformation("Got {NumberOfVariables} variables for API key {ApiKeyId}", apiKeyId);
 
-        return variables;
+        return _serializer.SerializeToJson(variablesOfApiKeyWithHighestSpecificity);
     }
 
     public async Task SaveAsync(VariablesEditModel model, CancellationToken cancellationToken)
     {
         await using var dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
 
-        _logger.LogDebug("Saving variables for tags {@TagIds}", model.TagIds);
-        
-        // Get tags
-        var tags = await dbContext.Tags
-            .Where(tag => model.TagIds.Contains(tag.Id))
-            .AsNoTracking()
-            .ToListAsync(cancellationToken);
-        
-        // Get existing variables
-        var existingVariables = await dbContext.TagVariables
-            .Where(tv => model.TagIds.Contains(tv.TagId))
-            .GroupBy(tv => tv.VariableId)
-            .Where(g => g.Count() == tags.Count)
-            .Select(tv => tv.Key)
-            .Join(dbContext.Variables,
-                variableId => variableId,
-                variable => variable.Id,
-                (variableId, variable) => variable)
-            .AsTracking()
-            .ToListAsync(cancellationToken);
-        
-        var newVariables = _deserializer.DeserializeFromJson(model.Json);
-        
-        
-        
-        
+        var tagIds = model.TagIds;
+        var applicationIds = model.ApplicationIds;
+        _logger.LogDebug("Saving variables for applications {@ApplicationIds} and tags {@TagIds}", applicationIds, tagIds);
 
+        var variables = dbContext.Variables;
+        var tagVariables = dbContext.TagVariables;
+        var applicationVariables = dbContext.ApplicationVariables;
+        
+        IQueryable<VariableRecord> existingVariables = variables;
+        
+        existingVariables = tagIds.Count == 0
+            // Take variables not linked to any tags
+            ? existingVariables.Where(v => !tagVariables.Any(tv => tv.VariableId == v.Id)) 
+            // Take variables linked to this exact combination of tags
+            : existingVariables.Where(v => tagVariables.Count(tv => tv.VariableId == v.Id && tagIds.Contains(tv.TagId)) == tagVariables.Count(tv => tv.VariableId == v.Id));
+        
+        existingVariables = applicationIds.Count == 0
+            // Take variables not linked to any applications
+            ? existingVariables.Where(v => !applicationVariables.Any(av => av.VariableId == v.Id)) 
+            // Take variables linked to this exact combination of applications
+            : existingVariables.Where(v => applicationVariables.Count(tv => tv.VariableId == v.Id && applicationIds.Contains(tv.ApplicationId)) == applicationVariables.Count(tv => tv.VariableId == v.Id));
+
+        var existingVariablesByKey = (await existingVariables.ToListAsync(cancellationToken)).ToDictionary(v => v.Key);
+        var newVariables = _deserializer.DeserializeFromJson(model.Json);
+
+        // Add or update variables
+        foreach (var newVariable in newVariables)
+        {
+            var key = newVariable.Key;
+            var value = newVariable.Value;
+            var valueType = newVariable.ValueType;
+
+            if (existingVariablesByKey.Remove(key, out var variableRecord))
+            {
+                if (!string.Equals(value, variableRecord.Value)
+                    || valueType != variableRecord.ValueType)
+                {
+                    variableRecord.Value = value;
+                    variableRecord.ValueType = valueType;
+                    variableRecord.UpdatedAtUtc = DateTime.UtcNow;
+                }
+            }
+            else
+            {
+                variableRecord = new VariableRecord
+                {
+                    Key = key,
+                    Value = value,
+                    ValueType = valueType,
+                    CreatedAtUtc = DateTime.UtcNow,
+                    UpdatedAtUtc = DateTime.UtcNow
+                };
+                variables.Add(variableRecord);
+            }
+        }
+        
+        // Delete old variables
+        foreach (var deletedVariable in existingVariablesByKey.Values)
+        {
+            variables.Remove(deletedVariable);
+        }
+
+        await dbContext.SaveChangesAsync(cancellationToken);
     }
 }
 
