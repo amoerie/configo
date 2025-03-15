daff2cd6f06072c8a503f9801e9975dd829768cc
Alexander Moerman <alexander.moerman@gmail.com>
2025-03-15 10:05:28 +0100

    Add AuthenticationOptions

---
+++ src/Configo.Server/AuthenticationOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Configo.Server;
+
+public sealed record AuthenticationOptions
+{
+    public required MicrosoftOptions Microsoft { get; set; }
+}
+
+public sealed record MicrosoftOptions
+{
+    public required string ClientId { get; set; }
+    public required string ClientSecret { get; set; }
+}
