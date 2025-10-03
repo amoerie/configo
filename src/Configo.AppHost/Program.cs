42a7f113ce0b6e4b387cd1af4dc575cb11f49ae7
Alexander Moerman <alexander.moerman@gmail.com>
2025-10-03 20:34:18 +0200

    Add postgres to Aspire host

--- src/Configo.AppHost/Program.cs
+++ src/Configo.AppHost/Program.cs
@@ -1,5 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Configo_Server>("server");
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume()
+    .AddDatabase("configodb");
+
+builder.AddProject<Projects.Configo_Server>("server")
+    .WithReference(postgres);
 
 builder.Build().Run();
